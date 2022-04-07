b72b80845b7df816800c8372de9e7113cc7710c3
tarena <1139391290@qq.com>
2022-04-07 09:50:21 +0800

    SpringMVC拦截器

--- springmvc01/src/main/java/cn/tedu/springmvc/config/SpringMvcConfig.java
+++ springmvc01/src/main/java/cn/tedu/springmvc/config/SpringMvcConfig.java
@@ -3,10 +3,18 @@ package cn.tedu.springmvc.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration // 此注解不是必须的
 @EnableWebMvc
 @ComponentScan("cn.tedu.springmvc") // 必须配置在当前配置类，不可配置在Spring的配置类
 public class SpringMvcConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        WebMvcConfigurer.super.addInterceptors(registry);
+
+    }
 }
