a11ed6810a5e8a1588934872021e54944de80cd9
tarena <1139391290@qq.com>
2022-04-07 09:31:11 +0800

    程恒

---
+++ springmvc01/src/main/java/cn/tedu/springmvc/interceptor/LoginInterceptor.java
@@ -0,0 +1,26 @@
+package cn.tedu.springmvc.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("LoginInterceptor.preHandle");
+        return false;
+
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("LoginInterceptor.postHandle");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("LoginInterceptor.afterCompletion");
+    }
+}
